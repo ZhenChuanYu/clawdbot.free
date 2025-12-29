bf658e7e5b2ea12eae14d18abf7bae52acd22902
yuzhenchuan <yuzhenchuan@yuzhenchuandeMacBook-Pro.local>
2025-12-30 03:52:22 +0900

    save

--- .puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const { join } = require('path');
-
-/**
- * Puppeteer 配置
- * 用于 Cloudflare Pages 构建环境
- */
-module.exports = {
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
--- src/main.tsx
+++ src/main.tsx
@@ -5,24 +5,10 @@ import App from './App'
 import './i18n/config'
 import './index.css'
 
-const container = document.getElementById('root')!
-
-// react-snap 需要使用 hydrate 来接管预渲染的 HTML
-if (container.hasChildNodes()) {
-  ReactDOM.hydrateRoot(
-    container,
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  )
-} else {
-  ReactDOM.createRoot(container).render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  )
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
